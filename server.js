0058e7e6d89fa101445726577fadf1686b923368
Safiyya Sharif <SafiyyaSharif@safiyyas-air.attlocal.net>
2017-08-09 00:54:41 -0400

    updated file

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
 
 
 // parse application/x-www-form-urlencoded 
